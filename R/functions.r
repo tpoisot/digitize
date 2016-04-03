043b9a9cd67cfd22815db218c9a67a0b77bcf87a
Tal Galili <tal.galili@gmail.com>
2016-04-03 15:35:06 +0300

    Added the "digitize" function
    The "digitize" function guides the user to perform all steps in one go.

--- R/functions.r
+++ R/functions.r
@@ -43,3 +43,17 @@ Calibrate <- function(data, calpoints, x1, x2, y1, y2)
   
   return(as.data.frame(data))
 }
+
+
+#' @export
+digitize <- function(fname, x1, x2, y1, y2, ...)
+{
+  cat("You must start by clicking on the left-most x-axis point, then the right-most axis point, followed by the lower y-axis point and finally the upper y-axis point. You don’t need to choose the end points of the axis, only two points on the axis that you know the x or y value for. As you click on each of the 4 points, the coordinates are saved in the object cal.")
+  calpoints <- ReadAndCal(fname)
+  cat("Now you can click on each of the data points you’re interested in retrieving values for. The function will place a dot (colored red in this case) over each point you click on, and the raw x,y coordinates of that point will be saved to the data.points list. When you’re finished clicking points, you need to hit stop or right-click to stop the data point collection.")
+  data <- DigitData()
+  Calibrate(data, calpoints, x1, x2, y1, y2)
+}
+
+
+
