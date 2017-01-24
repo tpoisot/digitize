9f7c5c1f8d335dc6a3e07378c4b847a6e9dafd2a
Jaime Ashander <jashander@ucdavis.edu>
2017-01-24 15:28:37 -0800

    Add link in package help

--- R/digitize-package.r
+++ R/digitize-package.r
@@ -2,7 +2,7 @@
 #'
 #' Get data from a graph by providing calibration points
 #'
-#' The package provides one main function, `digitize`, which runs functions
+#' The package provides one main function, \code{\link{digitize}}, which runs functions
 #' that 1) Read the image in and calibrate it, and 2) Digitize the data.
 #' The first step requires user input.
 #'
