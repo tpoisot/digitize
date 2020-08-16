15f2c04a62ba51bb7dce4f2536220ef7dbe99825
Michael Davidson <michd@mit.edu>
2020-08-15 17:09:37 -0700

    Auto-digitize
    - added auto-digitization option to `digitize` function
    - uses `magick`
    - currently works on high-contrast single-line plots

--- R/functions.r
+++ R/functions.r
@@ -1,6 +1,6 @@
-#' (deprecated) Read image and calibrate
+#' (internal) Read image and calibrate
 #' @param fname Filename of the graphic to read
-#' @details deprecated, use \code{\link{digitize}} instead. Called for side effect of user locating points. See
+#' @details internal, use \code{\link{digitize}} instead. Called for side effect of user locating points. See
 #' `graphics::locator` for more. Usage explained at
 #' http://lukemiller.org/index.php/2011/06/digitizing-data-from-old-plots-using-digitize/
 #' @return `calpoints` List of the x and y coordinates of the calibration points
@@ -43,11 +43,11 @@ ReadImg <- function(fname)
   rasterImage(img, 0, 0, 1, 1)
 }
 
-#' (deprecated) Mark the data on an image
+#' (internal) Mark the data on an image
 #' @param col color of marker as in `par`
 #' @param type shape of marker as in `par`
 #' @param ... other args for `locator`
-#' @details deprecated, use \code{\link{digitize}} instead. This function waits for the user to click the points of
+#' @details internal, use \code{\link{digitize}} instead. This function waits for the user to click the points of
 #' the coordinates. See `graphics::locator` for more. Usage explained at
 #' http://lukemiller.org/index.php/2011/06/digitizing-data-from-old-plots-using-digitize/
 #' @return `data` A list with the coordinates of the points
@@ -60,14 +60,14 @@ DigitData <- function(col = 'red', type = 'p', ...)
   locator(type = type, col = col, ...)
 }
 
-#' (deprecated) Digitize the data
+#' (internal) Calibrate the data
 #' @param data output of `DigitData`
 #' @param calpoints output of `ReadAndCal`
 #' @param x1 X-coordinate of the leftmost x point (corrected)
 #' @param x2 X-coordinate of the rightmost x point (corrected)
 #' @param y1 Y-coordinate of the lower y point (corrected)
 #' @param y2 Y-coordinate of the upper y point (corrected)
-#' @details deprecated, use \code{\link{digitize}} instead. This function corrects the data according to the
+#' @details internal, use \code{\link{digitize}} instead. This function corrects the data according to the
 #' calibration information. Usage further explained at
 #' http://lukemiller.org/index.php/2011/06/digitizing-data-from-old-plots-using-digitize/
 #' @return `data` A data frame with the corrected coordinates of the points
@@ -94,6 +94,7 @@ Calibrate <- function(data, calpoints, x1, x2, y1, y2, twopoints = F)
   
   return(as.data.frame(data))
 }
+
 
 getVals <- function(names) {
   vals <- list()
@@ -185,12 +186,13 @@ instructCal = function(pt_names, twopoints=F) {
 #' @param y1 (optional) the lower y-axis point
 #' @param y2 (optional) the upper y-axis point
 #' @param twopoints (optional) if true calibrate with two points instead of four. Defaults to FALSE.
+#' @param auto (optional) if true use automatic digitization. Defaults to FALSE.
 #' @param  ... pass parameters col or type to change data calibration points
-#' @details Proceeds in two steps, both of which require user input
+#' @details Manual procedure (auto=F) proceeds in two steps, both of which require user input
 #'          from the mouse:
-#'
+#'          
 #'            1) Read the image in and calibrate it
-#'
+#'          
 #'            2) Digitize the data
 #'
 #'          Calibration points are optionally passed via arguments x1, x2, y1,
@@ -199,7 +201,20 @@ instructCal = function(pt_names, twopoints=F) {
 #'          If not specified, you are prompted to enter these in the
 #'          console. Note, you don’t need to choose the end points of each axis,
 #'          only two points for which you know the x or y return.
-#' @return a data.frame containing the digitized data
+#'
+#'          Automated procedure (auto=T) requires user input in first step only:
+#'          
+#'            1) Read the image in and calibrate it by clicking on the bounds of the plot
+#'          
+#'            2) Automatically digitize high-contrast data within the plotting region
+#'          
+#'          Note: currently auto-digitize does not accommodate multiple lines within plotting region
+#'          
+#' @return  data.frame containing the digitized data (auto=F)
+#' 
+#'             OR
+#'             
+#'          spline function as output from splinefun() (auto=T)
 #' @examples
 #' \dontrun{
 #' tmp <- tempfile()
@@ -207,7 +222,7 @@ instructCal = function(pt_names, twopoints=F) {
 #' plot(rnorm(10) + 1:10, xlab="x", ylab="y")
 #' dev.off()
 #'
-#' mydata <- digitize(tmp)
+#' mydata <- digitize(tmp, auto=T)
 #' }
 #' @importFrom utils flush.console
 #' @export
@@ -217,7 +232,8 @@ digitize <- function(image_filename,
                     x2,
                     y1,
                     y2,
-                    twopoints = F) {
+                    twopoints = F,
+                    auto = F) {
   if (twopoints) {
     pt_names <- c("x1y1", "x2y2")
     instructCal(pt_names, twopoints = twopoints)
@@ -231,44 +247,76 @@ digitize <- function(image_filename,
   
   cal <- ReadAndCal(image_filename, twopoints = twopoints)
   
-  if (twopoints) {
-    # can pass only partial limits to digitize function, and get the rest from user input
-    if (missing(x1)) {
-      x1 <- getVals('x1')[['x1']]  }
-    if (missing(x2)) {
-      x2 <- getVals('x2')[['x2']]  }
-    if (missing(y1)) {
-      y1 <- getVals('y1')[['y1']]  }
-    if (missing(y2)) {
-      y2 <- getVals('y2')[['y2']]  }
+  # fill in points not passed through function
+  if (missing(x1)) {
+    x1 <- getVals('x1')[['x1']]  }
+  if (missing(x2)) {
+    x2 <- getVals('x2')[['x2']]  }
+  if (missing(y1)) {
+    y1 <- getVals('y1')[['y1']]  }
+  if (missing(y2)) {
+    y2 <- getVals('y2')[['y2']]  }
+
+  cat("\n\n")
+  
+  if (auto) {
+    # automatically read graph
+    cat(
+      ".....AUTOMATED INPUT.....",
+      "Attempting to use `magick` to extract curve",
+      sep = "\n\n"
+    )    
+
+    # Read in image using `magick` and extract lines with high saturation
+    #   more info on magick package: https://www.r-bloggers.com/extracting-the-data-from-static-images-of-graphs-with-magick/
+    im <- image_read(image_filename) %>% 
+        image_quantize() %>%
+        image_median(radius=5) %>%
+        image_threshold("white", "30%") %>%
+        image_channel("saturation") %>%
+        image_negate()
+
+    # bounds of image we wish to digitize (i.e., within axes)
+    im.bounds <- Calibrate(cal, 
+                           list(x=c(0,1), y=c(0,1)),
+                           1, image_info(im)$width, 1, image_info(im)$height,
+                           twopoints = twopoints)
+
+    data <- image_data(im)[1,,] %>%
+      as.data.frame() %>%
+      mutate(Row = 1:nrow(.)) %>%
+      select(Row, everything()) %>%
+      mutate_all(as.character) %>%
+      gather(key = Column, value = value, 2:ncol(.)) %>%
+      mutate(Column = as.numeric(gsub("V", "", Column)),
+             x = as.numeric(Row),
+             value = ifelse(value == "00", NA, 1),
+             y = max(Column)-Column) %>%  # reverse y-axis to start from bottom-right
+      filter(!is.na(value)) %>%
+      filter(x >= min(im.bounds$x), x <= max(im.bounds$x), 
+             y >= min(im.bounds$y), y <= max(im.bounds$y)) %>%
+      select(x,y)
+    
+    data.line <- Calibrate(data, as.list(im.bounds), x1, x2, y1, y2, twopoints = twopoints)
+    out <- splinefun(data.line$x, data.line$y, method = "fmm")
+    
   } else {
-    missing_pt <- missing(x1) | missing(x2) | missing(y1) | missing(y2)
-    if (missing_pt) {
-      ## I would abstract this into a seperate function but the assign
-      ## below magics the vars x1, ..y2 into their appropriate vals
-      ## and need to deal with environments to do that...
-      point_vals <- getVals(pt_names)
-      for (p in names(point_vals))
-        assign(p, point_vals[[p]])
-    }
+    cat(
+      ".....MANUAL INPUT.....",
+      "Click all the data. (Do not hit ESC, close the window or press any mouse key.)",
+      "Once you are done - exit:",
+      " - Windows: right click on the plot area and choose 'Stop'!",
+      " - X11: hit any mouse button other than the left one.",
+      " - quartz/OS X: hit ESC",
+      sep = "\n\n"
+    )
+    cat("\n\n")
+    flush.console()
+  
+    data <- DigitData(...)
+    out <- Calibrate(data, cal, x1, x2, y1, y2, twopoints = twopoints)
+    row.names(out) <- NULL
   }
   
-  cat("\n\n")
-  cat(
-    "..............NOW .............",
-    "Click all the data. (Do not hit ESC, close the window or press any mouse key.)",
-    "Once you are done - exit:",
-    " - Windows: right click on the plot area and choose 'Stop'!",
-    " - X11: hit any mouse button other than the left one.",
-    " - quartz/OS X: hit ESC",
-    sep = "\n\n"
-  )
-  cat("\n\n")
-  flush.console()
-  
-  data <- DigitData(...)
-  
-  out <- Calibrate(data, cal, x1, x2, y1, y2, twopoints = twopoints)
-  row.names(out) <- NULL
   return(out)
 }
